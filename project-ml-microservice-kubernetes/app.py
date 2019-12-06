4d965f0c2f9d5633ee828910dbfca04853ffc6c8
emarchuet <emarchuet@gmail.com>
2019-12-06 19:44:49 +0100

    Update app.py

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
     #First log command
-    LOG.info(f"Scaling Payloadqqqqqqqqqqqqqqqqq: \n%s", payload)
+    #LOG.info(f"Scaling Payloadqqqqqqqqqqqqqqqqq: \n%s", payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
